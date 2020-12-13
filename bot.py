9699f3ec860f3e1251d68f5a8a686990ede68172
Matthew Stephenson <mrsteph3@asu.edu>
2020-12-13 02:46:01 -0700

    Comments and documentation.

--- bot.py
+++ bot.py
@@ -1,27 +1,33 @@
 import os
-import random
-import requests
 
 import discord
-from dotenv import load_dotenv
+import requests
 from discord.ext import commands
+from dotenv import load_dotenv
 
+'''
+Audiophyle: A Discord bot intended to retrieve information of songs such as artist, title, album name, and release date.
+Written by: Matthew Stephenson
+GitHub: /mrsteph3
+LinkedIn: /in/matthew-r-stephenson
+'''
+
+# Pull discord bot token from local environment variable.
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='$')
+# Initialize bot with specified command prefix.
+bot = commands.Bot(command_prefix='>>')
 
-random.seed()
 
+# Bot 'Commands' and 'Events'
+
+# This is a console output to signify that the bot is running and ready to process events/commands.
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command(name='test', help='Responds with a random number from 0 to 10 (inclusive).')
-async def test(ctx):
-    response = random.randint(0,10)
-    await ctx.send(str(response))
-
+# This is currently an echo command where the bot will respond when called using >>s [arg0, arg1, ...]
 @bot.command(name='s', help='Search for information on a song with a query {$s [query]}')
 async def s(ctx, *args):
     argument = ' '.join(args)
